14d85b59719ed20ee5dacfcff6c7782e0315a4f0
John Turner <fenwayriffs@gmail.com>
2016-01-13 16:47:54 -0500

    integration test for valid update of person

--- spec/features/user_creates_person_spec.rb
+++ spec/features/user_creates_person_spec.rb
@@ -12,6 +12,11 @@ end
 
 feature "user edits existing person" do
   scenario "with valid data" do
-    pending "write your spec here"
+    person = Person.create(first_name: "Drusilla")
+    visit edit_person_path(person)
+    fill_in "person_first_name", with: "Helga"
+    click_button "Update Person"
+
+    expect(page).to have_content("Person updated.")
   end
 end
